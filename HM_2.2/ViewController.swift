bae0a9d51a9875450e021fca0523d13d6856e0c1
Elena <aalazuka@gmail.com>
2020-08-24 08:51:32 +0300

    Initial Commit

---
+++ HM_2.2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HM_2.2
+//
+//  Created by Елена Лазука on 24.08.2020.
+//  Copyright © 2020 Елена Лазука. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
